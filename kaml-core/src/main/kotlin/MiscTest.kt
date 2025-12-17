4fdf9ea1b1d578dce33205e7b4cf80ef3373eb1a
Seepick <seepick@users.noreply.github.com>
2025-12-17 20:02:34 +0100

    cleanup

--- kaml-core/src/main/kotlin/MiscTest.kt
+++
@@ -1,9 +0,0 @@
-import com.github.seepick.kaml.core.fixDashPlacement
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.equals.shouldBeEqual
-
-class MiscTest : StringSpec( {
-    "fixDashPlacement" {
-        fixDashPlacement("  -\n    name: X") shouldBeEqual "  - name: X"
-    }
-})
